feaee2b73434079cb5f6bda9111ce162985b8cb3
Kate Zaharova <15071981ktz@gmail.com>
2023-09-11 23:12:25 +0300

    1-st commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,84 @@
 import { Layout } from "./Layout";
 import { GlobalStyle } from "./GlobalStyle";
-import { Component } from "react";
+//import { Component } from "react";
 import { ContactForm } from "./Form/Form";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { nanoid } from 'nanoid';
+import { useState, useEffect } from "react";
 
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', contact:{ name: 'Rosie Simpson', phone: '459-12-56' } },
-      { id: 'id-2', contact:{ name: 'Hermione Kline', phone: '443-89-12' } },
-      { id: 'id-3', contact:{ name: 'Eden Clements', phone: '645-17-79' } },
-      { id: 'id-4', contact:{ name: 'Annie Copeland', phone: '227-91-26' } },
-  ],
-  filter: '',
-  };
+const getIntialFilters = () => {
+  const savedFilters = localStorage.getItem("contact-filter");
+  if (savedFilters !== null) {
+    return JSON.parse(savedFilters);
+  }
+  return '';
+};
 
 
-componentDidMount() {
-    const savedFilter = localStorage.getItem("contact-filter");
-      if (savedFilter !== null) {
-      this.setState({
-        filter: JSON.parse(savedFilter)
-      });
-    }
-}
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.filter !== this.state.filter) {
-      localStorage.setItem("contact-filter", JSON.stringify(this.state.filter))
-}    
-}
+const getIntialContacts = () => {
+  const savedContacts = localStorage.getItem("contactBook");
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  }
+  return [
+      { id: 'id-1', contact: { name: 'Rosie Simpson', phone: '459-12-56' } },
+      { id: 'id-2', contact: { name: 'Hermione Kline', phone: '443-89-12' } },
+      { id: 'id-3', contact: { name: 'Eden Clements', phone: '645-17-79' } },
+      { id: 'id-4', contact: { name: 'Annie Copeland', phone: '227-91-26' } },
+  ];
+};
 
 
-  addContact = contact => {
-    console.log(contact)
-    if (-1 !== this.state.contacts.findIndex(option => option.contact.name === contact.name)) {
-      alert(`${contact.name} is already in contacts.`);
-      return;
-    }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), contact: contact }],
-    }))
-  };
- 
-  
-  deleteContact = contactId => {
-    console.log(contactId);
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(item => item.id !== contactId),
-    }));
-  };
+
+export const App = () => {
+  const [contacts, setContacts] = useState(getIntialContacts());
+  const [filter, setFilter] = useState(getIntialFilters());
 
 
-  findName = filterName => {
-    this.setState(() => ({
-      filter: filterName
-    })
-    ); 
-  };
+  useEffect(() => {
+    localStorage.setItem("contact-filter", JSON.stringify(filter))
+  }, [filter]);
 
-  getFilteredContacts= () => {
-    const { contacts, filter } = this.state;
 
-    return contacts.filter(item => {
+    useEffect(() => {
+    localStorage.setItem("contactBook", JSON.stringify(contacts))
+  }, [contacts]);
+
+
+  const filteredContacts = contacts.filter(item => {
       return item.contact.name
         .toLowerCase()
         .includes(filter.toLowerCase());
     });
+
+
+const addContact = contact => {
+    if (-1 !== contacts.findIndex(option => option.contact.name === contact.name)) {
+      alert(`${contact.name} is already in contacts.`);
+      return;
+    }
+    setContacts(prevItems => [...prevItems, { id: nanoid(), contact: contact }])
   };
 
- resetFilter = () => {
-    this.setState({
-      filter: '',
-    })
-  }
+
+const deleteContact = contactId => {
+    setContacts(prevItems => prevItems.filter(item => item.id !== contactId)
+    );
+  };
 
 
-  render() {
-    const filteredContacts = this.getFilteredContacts();
+const findName = filterName => {
+    setFilter(filterName); 
+  };
+
+
+  const resetFilter = () => {
+    setFilter('')
+  };
+
+
 
 return (
     <Layout
@@ -93,12 +91,12 @@ return (
         color: '#010101'
       }}>
     <h1>Phonebook</h1>
-    <ContactForm onAddContact={this.addContact} />
+    <ContactForm onAddContact={addContact} />
     <h2>Contacts</h2>
-    <Filter onChangeFilter={this.findName} onReset={this.resetFilter}/>
-    <ContactList contacts={filteredContacts} onDelete={this.deleteContact} contactData={this.contacts} />
+    <Filter onChangeFilter={findName} onReset={resetFilter} initialValue={filter} />
+    <ContactList contacts={filteredContacts} onDelete={deleteContact} contactData={contacts} />
     <GlobalStyle/>
     </Layout>
   );
+
   };
-};
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,12 +1,13 @@
 import { Wrapper, TextInput, ResetBtn } from "./Filter.styled";
 
-export const Filter = ({onChangeFilter, onReset}) => {
+export const Filter = ({onChangeFilter, onReset, initialValue}) => {
     return (
         <Wrapper>
              <label>Find contacts by name</label>
              <TextInput
                 type="text"
                 name="Findname"
+                value={initialValue}
                 onChange={evt=>onChangeFilter(evt.target.value)}
             />
              <ResetBtn onClick={onReset}>Reset</ResetBtn>
